16d8a1f298d60da3da2e4112a85730a778ca718f
Pilot2254 <Pilot2254.gamer@gmail.com>
2023-12-01 16:28:19 +0100

    skyro update

--- invite/script.js
+++
@@ -1,9 +0,0 @@
-// Redirect function
-function redirectToWelcome() {
-    window.location.href = "https://discord.gg/QWVzKMfQfz";
-}
-
-// Automatically redirect after the page loads
-window.onload = function () {
-    redirectToWelcome();
-};
